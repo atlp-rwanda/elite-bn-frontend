20cf9dac9531707c23f286901d183253016c23b3
pextech <mcstain1639@gmail.com>
2021-01-19 13:37:36 +0200

    Updated Barefoot.png

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import img from '../../barefoot.png'
 const Index = () => (
   <div className="text-center">
     <h2 className="m-6">
-      Welcome to Our Very beginning of this Barefoot Fron-end Implementation
+      Welcome to Our Very beginning of this Barefoot Front-end Implementation
     </h2>
     <img src={img} alt="Barefoot"></img>
   </div>
